34d01dfc3768aa7ae6fb40515cc2abff18720bbd
Dan Hill <dhill@promoted.ai>
2024-10-09 16:19:41 -0700

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,29 @@
-// eslint.config.js
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
 export default [
     {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        }
+    },
+    ...tseslint.configs.recommended,
+    {
+        files: [
+            "src/**/*.ts",
+            "src/**/*.test.ts",
+        ],
         ignores: [
             "build/",
             "dist/",
             "node_modules/",
             ".snapshots/",
             "*.min.js"
-        ]
+        ],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off"
+        }
     }
 ];
