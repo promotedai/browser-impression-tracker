9edfcc65ba063ab8d6877e521682d8dc6c1feb77
Dan Hill <dhill@promoted.ai>
2024-10-09 16:03:18 -0700

    fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// eslint.config.js
+export default [
+    {
+        ignores: [
+            "build/",
+            "dist/",
+            "node_modules/",
+            ".snapshots/",
+            "*.min.js"
+        ]
+    }
+];
